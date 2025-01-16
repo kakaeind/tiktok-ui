d864866405aa85ca05799ad5d3f4039eac5472ac
HaQuangVinh <vinhlc004@gmail.com>
2025-01-16 10:57:03 +0700

    Logic Header #1

--- src/components/Accountitem/index.js
+++ src/components/Accountitem/index.js
@@ -2,12 +2,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import classNames from "classnames/bind";
 import styles from "./AccountItem.module.scss";
-
+import Image from "~/components/Image";
 const cx = classNames.bind(styles);
 function AccountItem() {
   return (
     <div className={cx("wrapper")}>
-      <img
+      <Image
         className={cx("avatar")}
         src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/d4579bde29a0510cc919d515af4b35fe~c5_300x300.webp?lk3s=a5d48078&nonce=29205&refresh_token=98c7350c346c0a00cc821d8d5ca982f8&x-expires=1736989200&x-signature=Vk6GdMO791cMqcYpaTSc6Kj2PGE%3D&shp=a5d48078&shcp=c1333099"
         alt="Duongdomic"
---
+++ src/components/Layout/components/Search/index.js
@@ -0,0 +1,80 @@
+import {
+    faCircleXmark,
+    faSpinner,
+    
+} from "@fortawesome/free-solid-svg-icons";
+import HeadlessTippy from "@tippyjs/react/headless";
+import { Wrapper as PopperWrapper } from "~/components/Popper";
+import AccountItem from "~/components/Accountitem";
+import {  SearchIcon } from "~/components/Icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import styles from './Search.module.scss'
+import {  useEffect, useState, useRef } from "react";
+
+
+const cx = classNames.bind(styles)
+function Search() {
+  const [searchValue,setSearchValue] = useState('')
+    const [searchResult, setSearchResult] = useState([]);
+  const [showResult,setShowResult] = useState(true)
+  const inputRef = useRef()
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult([1,2,3 ]);
+    }, 0);
+  }, []);
+
+  const handleClear =()=>{
+    setSearchValue('')
+    setSearchResult([])
+    inputRef.current.focus()
+  }
+
+  const handleHideResult =()=>{
+setShowResult(false)
+  } 
+  
+    return (  
+        <HeadlessTippy
+          interactive
+          visible={showResult && searchResult.length > 0}
+          render={attrs =>
+            <div className={cx("search-result")} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                <h4 className={cx("search-title")}>Account</h4>
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+              </PopperWrapper>
+            </div>}
+       onClickOutside={handleHideResult}
+        >
+          <div className={cx("search")}>
+            <input
+            ref={inputRef}
+            value={searchValue} 
+            placeholder="Search account and videos" spellCheck={false} 
+            onChange={(e) => setSearchValue(e.target.value)} 
+            onFocus={() => setShowResult(true) }
+            />
+           
+            {!!searchValue && (
+
+            <button className={cx("clear")} 
+            onClick={handleClear}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            )}
+            {/* <FontAwesomeIcon className={cx("loading")} icon={faSpinner} /> */}
+
+            <button className={cx("search-btn")}>
+              <SearchIcon/>
+            </button>
+          </div>
+        </HeadlessTippy>);
+}
+
+export default Search;
